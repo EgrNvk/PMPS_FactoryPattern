e87107e1f2994c866876e912c9dcba7644489a4d
EgrNvk <egrnvk@gmail.com>
2026-02-16 14:31:55 +0200

    Added usage in concrete coffee shop

--- Factory.py
+++ Factory.py
@@ -1,40 +1,37 @@
 from abc import ABC, abstractmethod
 
 class Coffee(ABC):
-
     @abstractmethod
     def prepare(self):
         pass
 
 class Espresso(Coffee):
-
     def prepare(self):
         print("Готується еспресо")
 
-
 class Latte(Coffee):
-
     def prepare(self):
         print("Готується латте")
 
 class CoffeeShop(ABC):
-
     def order_coffee(self):
         coffee = self.create_coffee()
         coffee.prepare()
-
     @abstractmethod
     def create_coffee(self):
         pass
 
-
 class ItalianCoffeeShop(CoffeeShop):
-
     def create_coffee(self):
         return Espresso()
 
-
 class FrenchCoffeeShop(CoffeeShop):
-
     def create_coffee(self):
         return Latte()
+
+print("В італійскій кав'ярні")
+shop1 = ItalianCoffeeShop()
+shop1.order_coffee()
+print("В французкій кав'ярні")
+shop2 = FrenchCoffeeShop()
+shop2.order_coffee()
