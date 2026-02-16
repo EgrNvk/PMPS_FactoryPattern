40abae76ea267cad44f80adbe9de19edf55dbd0d
EgrNvk <egrnvk@gmail.com>
2026-02-16 14:24:49 +0200

    Created an abstract class CoffeeShop and several concrete coffee shop classes

--- Factory.py
+++ Factory.py
@@ -16,3 +16,25 @@ class Latte(Coffee):
 
     def prepare(self):
         print("Готується латте")
+
+class CoffeeShop(ABC):
+
+    def order_coffee(self):
+        coffee = self.create_coffee()
+        coffee.prepare()
+
+    @abstractmethod
+    def create_coffee(self):
+        pass
+
+
+class ItalianCoffeeShop(CoffeeShop):
+
+    def create_coffee(self):
+        return Espresso()
+
+
+class FrenchCoffeeShop(CoffeeShop):
+
+    def create_coffee(self):
+        return Latte()
